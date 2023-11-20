3bc1ee93200fc9a866dac3d14c757713c5db8932
Chimara Okeke <123244902+Echimara@users.noreply.github.com>
2023-11-20 11:44:20 -0600

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_PVByLVh6h', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1d48ge2f6soa2ggidker9ctdjr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://ri4wzg5nnd.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
